3b8d2e53568f28f8973830d992243efc4e698350
ituitis-kavraz19 <55850855+ituitis-kavraz19@users.noreply.github.com>
2020-03-23 12:38:41 +0300

    Delete sadeceRakamlar.py

--- sadeceRakamlar.py
+++
@@ -1,9 +0,0 @@
-#liste içinde sadece rakamlardan oluşan elemanları bulmak
-
-liste = ["345","sadas","324a","14","kemal"]
-for a in liste:
-    try:
-        print(int(a))
-    except:
-        pass
-
