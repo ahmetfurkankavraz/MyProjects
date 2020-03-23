08e1acd68b1de9166ad8a729a45a988d2e6a1733
ituitis-kavraz19 <55850855+ituitis-kavraz19@users.noreply.github.com>
2020-03-23 12:31:59 +0300

    Create ortakebob.py

---
+++ sayıçalışmaları/ortakebob.py
@@ -0,0 +1,18 @@
+# iki sayının ebobunu bulmak için yazılan bir fonksiyon
+
+def ortakebob(a, b):
+    liste = []
+    if (a<b):
+        z = a + 1 
+        for i in range(1, z):
+            if (b % i == 0  and a % i == 0):
+                liste += [i]
+
+    else:
+        z = b + 1 
+        for i in range(1, z):
+            if (b % i == 0  and a % i == 0):
+                liste += [i]
+    print(liste[-1])
+
+ortakebob(320, 200)
